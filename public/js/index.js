48f3dfda8b534f6a8dd164b522137001f788fec4
301602218048 <abhinav.sharma2901@gmail.com>
2025-07-01 17:02:15 +0530

    login frontend

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-const api = "http://localhost:3000";
+const api = "http://localhost:3000/user";
 const msg = document.getElementById("message");
 
 function handleSignUp(e) {
@@ -15,7 +15,30 @@ function handleSignUp(e) {
 
 async function addData(obj) {
   try {
-    const user = await axios.post(`${api}/user/signup`, obj);
+    const user = await axios.post(`${api}/signup`, obj);
+    if (user.data.success) {
+      alert(`${user.data.msg}`);
+    }
+  } catch (error) {
+    console.log(error);
+    updateDOM(error);
+  }
+}
+
+function handleLogin(e) {
+  e.preventDefault();
+  const obj = {
+    email: e.target.email.value,
+    password: e.target.password.value,
+  };
+  userLogin(obj);
+  e.target.reset();
+}
+
+async function userLogin(obj) {
+  try {
+    const user = await axios.post(`${api}/login`, obj);
+    console.log(user);
     if (user.data.success) {
       alert(`${user.data.msg}`);
     }
